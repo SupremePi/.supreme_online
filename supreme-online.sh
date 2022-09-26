45984c42d34fc562354d058e83b4094176e429b6
SupremePi <76017561+SupremePi@users.noreply.github.com>
2022-09-26 13:26:07 -0400

    Add files via upload

---
+++ supreme-online.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SUPREME ULTRA
+# The Supreme Team https://github.com/SupremePi/
+
+sb_version="Supreme Online Version 1"
+
+################
+#SUPREME ONLINE#
+################
+
+echo "test"
+sleep 3
