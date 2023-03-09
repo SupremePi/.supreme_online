d5580f5463148a0779dfc6a4b27825de5387f678
Supreme Retropie <76017561+SupremePi@users.noreply.github.com>
2023-03-08 23:32:53 -0500

    Update supreme-online.sh

--- supreme-online.sh
+++ supreme-online.sh
@@ -4,11 +4,11 @@
 
 #sb_version="Supreme Online Version 1"
 
-#echo "SUPREME NEWS! TEAM SPLIT!"
-#echo ""
-#echo "PLEASE UNFOLLOW THE SUPREME RETRO GAMING YOUTUBE AND PATREON THIS IS NOT US."
-#echo ""
-#sleep 1
+echo "SUPREME NEWS! TEAM SPLIT!"
+echo ""
+echo "PLEASE UNFOLLOW THE SUPREME RETRO GAMING YOUTUBE AND PATREON THIS IS NOT US."
+echo ""
+sleep 6
 
 
 ################
