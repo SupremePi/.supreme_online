8bd40cc9c271fb12d402423d65bd47b42ebdaee8
Supreme Retropie <76017561+SupremePi@users.noreply.github.com>
2023-11-24 19:02:12 -0500

    Update supreme_online_menu_raspberry_pi_5.sh

--- supreme_online_menu_raspberry_pi_5.sh
+++ supreme_online_menu_raspberry_pi_5.sh
@@ -2,6 +2,36 @@
 # SUPREME ULTRA
 # The Supreme Team https://github.com/SupremePi/
 
+sb_version="Supreme Online Raspberry Pi"
+sb_toolkit=/home/pi/.supreme_toolkit/sb_toolkit
+rp_menu=/home/pi/RetroPie/retropiemenu
+sb_menu=$sb_toolkit/retropiemenu
+sb_menu_pi5=$sb_toolkit/retropiemenu-pi5
+
+function quick_check() {
+if [ ! -d /home/pi/.supreme_toolkit ]; then
+    echo -e "$(tput setaf 2)Looks like the toolkit was not found on your build but no worries adding it now!$(tput sgr0)"
+    sleep 3
+    cd /home/pi/
+    git clone https://github.com/SupremePi/.supreme_toolkit.git
+    clear
+    cd $HOME/.supreme_toolkit
+    chmod +x -R ./
+    echo -e "$(tput setaf 2)Finished!$(tput sgr0)"
+    sleep 3
+    clear 
+    /home/pi/RetroPie/retropiemenu/supreme-online-toolkit.sh
+else
+main_menu
+fi
+}
+
+#Outside Links
+if [ -d /home/pi/.supreme_toolkit ]; then
+. /home/pi/.supreme_toolkit/sb_toolkit/_scripts/supreme-functions
+. /home/pi/.supreme_toolkit/sb_toolkit/_scripts/supreme-vars
+fi
+
 ################
 #SUPREME ONLINE#
 ################
@@ -33,19 +63,27 @@ function main_menu() {
             --menu "$sb_version" 25 75 20 \
             - "*** AVAILABLE ONLINE SELECTIONS ***" \
             - "" \
-	        1 " -  UPDATE/UPGRADE OS" \
+	        1 " -  ULTRA TOOL-KIT RESTORE (CLEAN INSTALL)" \
+			2 " -  ULTRA TOOL-KIT UPDATE CHECKER" \
+            3 " -  CHECK FOR ULTRA FIXES" \
+			4 " -  CHECK FOR UPDATES" \
+			5 " -  SUPREME INSTALLERS" \
 	        - "" \
-            2 " -  POWER OFF PI" \
-            3 " -  RESTART PI" \
+            6 " -  POWER OFF PI" \
+            7 " -  RESTART PI" \
 	        - "" \
-			4 " -  SUPREME CREDITS" \
+			8 " -  SUPREME CREDITS" \
             2>&1 > /dev/tty)
 
         case "$choice" in
-            1) ultra_restore  ;;
-	        2) supreme_off  ;;
-            3) supreme_restart  ;;
-			4) supreme_credits  ;;
+            1) supreme_restore  ;;
+            2) supreme_update  ;;
+			3) supreme_fixes  ;;
+			4) supreme_update  ;;
+			5) supreme_installers  ;;
+	        6) supreme_off  ;;
+            7) supreme_restart  ;;
+			8) supreme_credits  ;;
             -) none ;;
             *)  break ;;
         esac
@@ -53,13 +91,229 @@ function main_menu() {
 	clear
 }
 
-function ultra_restore() {
+
+function supreme_restore() {
+    #Reinstall Supreme Toolkit
+    echo -e "$(tput setaf 2)Now Restoring Supreme Toolkit And Updating Retropie Menus! $(tput sgr0)"
+	sleep 3
+    if [ -d /home/pi/.supreme_toolkit ]; then
+    sudo mv /home/pi/.supreme_toolkit /home/pi/.supreme_toolkit.bk
+    sleep 3
+	cd /home/pi/
+    git clone https://github.com/SupremePi/.supreme_toolkit.git
+	clear
+	cd $HOME/.supreme_toolkit
+	chmod +x -R ./
+    fi
+	if [ -d /home/pi/.supreme_toolkit ]; then
+    if (dialog --title "REMOVE!" --yesno "Would You Like To Remove The Supreme Toolkit Back Up To Save Space!" 0 0 )
+    then
+    sudo rm -R /home/pi/.supreme_toolkit.bk
+    fi
+    fi
+    echo -e "$(tput setaf 2)Now Restoring Supreme Retropie Menus From Tool-Kit! $(tput sgr0)"
+    sleep 3
+	
+	#Rsync RetroPie Menu files
+    if [ -f $rp_menu/raspiconfig.rp ]; then 
+	sudo rsync -av $rp_menu/raspiconfig.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/rpsetup.rp ]; then 
+	sudo rsync -av $rp_menu/rpsetup.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/showip.rp ]; then 
+	sudo rsync -av $rp_menu/showip.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/audiosettings.rp ]; then 
+	sudo rsync -av $rp_menu/audiosettings.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/bluetooth.rp ]; then 
+	sudo rsync -av $rp_menu/bluetooth.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/configedit.rp ]; then 
+	sudo rsync -av $rp_menu/configedit.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/esthemes.rp ]; then 
+	sudo rsync -av $rp_menu/esthemes.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/filemanager.rp ]; then 
+	sudo rsync -av $rp_menu/filemanager.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/retroarch.rp ]; then 
+	sudo rsync -av $rp_menu/retroarch.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/retronetplay.rp ]; then 
+	sudo rsync -av $rp_menu/retronetplay.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/runcommand.rp ]; then 
+    sudo rsync -av $rp_menu/runcommand.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/splashscreen.rp ]; then 
+	sudo rsync -av $rp_menu/splashscreen.rp $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/wifi.rp ]; then 
+	sudo rsync -av $rp_menu/wifi.rp $sb_menu_pi5/
+    fi
+	
+    #Rsync Custom Menu files
+    if [ -f $rp_menu/bezelproject.sh ]; then 
+    sudo rsync -av $rp_menu/bezelproject.sh $sb_menu_pi5/
+    fi
+    if [ -f $rp_menu/hurstythemes.sh ]; then 
+    sudo rsync -av $rp_menu/hurstythemes.sh $sb_menu_pi5/
+    fi
+    if [ -d $rp_menu/imp ]; then 
+    sudo rsync -av $rp_menu/imp $sb_menu_pi5/imp
+    fi
+	
+    #Cool Down
+    sleep 1
+    clear
+    echo "Now Fixing Supreme Menu."
+    sleep 3
+    clear
+    rm -rf $rp_menu/* && rsync -av $sb_menu_pi5/ $rp_menu && cp $sb_toolkit/retropie-gml-pi5/gamelist.xml /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+    sudo chown -R pi:pi $sb_menu_pi5/
+	sudo chmod 755 -R $sb_menu_pi5/
+	echo -e "$(tput setaf 2)Done! $(tput sgr0)"
+    sleep 3
+    clear
+}
+
+function supreme_update() {
+	#dialog --infobox "...Updating..." 3 20 ; sleep 1
+	clear
+	cd $HOME/.supreme_toolkit
+	echo
+	echo -e "$(tput setaf 2)Let's check that your on latest Supreme Tool kit... $(tput sgr0)"
+	echo
+	sleep 3
+	git fetch
+	git reset --hard HEAD
+	#git reset --hard && git clean -f -d
+	git merge '@{u}'
+	sleep 2
+	echo
+	find -name "*.sh" ! -name "joystick_selection.sh" -print0 | xargs -0 chmod 755
+	find -name "*.py" -print0 | xargs -0 chmod 755
+	find -name "*.rp" ! -name "raspiconfig.rp" ! -name "rpsetup.rp" | xargs sudo chown root:root
+    echo -e "$(tput setaf 2)Done! $(tput sgr0)"
+}
+
+function supreme_fixes() {
+	clear
+	echo -e "$(tput setaf 2)Now Downloading All Supreme Ultra Fixes! $(tput sgr0)"
+	sleep 3
+		
+	#Download Fixes
+	cd $HOME/
+    git clone https://github.com/SupremePi/.supreme_fixes.git
+    cd .supreme_fixes/
+
+	#Pi 5 changes if needed	
+	if [ "$rpi" = "5" ]; then
+	cd .supreme_fixes/pi-5
+    fi
+	
+	#Pi 3 changes if needed
+	if [ "$rpi" = "3" ]; then
+	sudo cp $HOME/.supreme_fixes/pi-3/boot/* $HOME/.supreme_fixes/boot/
+    fi
+	
+    #Sync All New Files		
+    rsync -urv --exclude '.git' --exclude 'boot' --exclude 'pi-3' --exclude '.gitattributes' --exclude 'opt' --exclude 'home' --exclude 'etc' --exclude 'var' --exclude 'usr' --exclude 'libretrocores' --exclude 'emulators' --exclude 'supplementary' --exclude 'LICENSE' --exclude 'README.md' --exclude 'roms' . /
+	sudo rsync -urv boot/ /boot/
+    sudo rsync -urv etc/ /etc/
+    sudo rsync -urv home/ /home/
+	sudo rsync -urv opt/ /opt/
+    sudo rsync -urv usr/ /usr/
+	echo -e "$(tput setaf 2)Done! $(tput sgr0)"
+	sleep 3
+	cd $HOME/
+	clear
+	
+	echo -e "$(tput setaf 2)Now Adding Ultra Fixes! Please Wait... $(tput sgr0)"
+	#Supreme fixes
+    sudo chown pi:pi -R $HOME/RetroPie/videoloadingscreens/
+	sudo chmod 755 -R $HOME/RetroPie/videoloadingscreens/	
+	
+	#Just incase user made changes
+	if [ ! -d $HOME/RetroPie/videoloadingscreens/default ]; then
+    mkdir $HOME/RetroPie/videoloadingscreens/default
+	fi
+	
+	#Quick User or Permission fix
+    sudo chmod +x /etc/profile.d/10-retropie* &> /dev/null
+    sudo chmod +x /etc/samba/smb.conf* &> /dev/null
+	sudo chown pi:pi -R /opt/retropie/configs/
+	sudo chown pi:pi -R /home/pi/
+    sleep 2
+    rm -rf ~/.supreme_fixes/
+	echo -e "$(tput setaf 2)Done! $(tput sgr0)"
+    sleep 3
+	clear
+}
+
+function supreme_update() {
        echo "Checking for updates..." 
        sleep 2 
        sudo apt update -y
        echo "Now upgrading packages..." 
        sleep 2
        sudo apt upgrade -y
+}
+
+function supreme_installers() {
+    local choice
+	
+    while true; do
+        choice=$(dialog --backtitle "$BACKTITLE" --title " SUPREME - INSTALLERS " \
+            --ok-label OK --cancel-label Exit \
+            --menu "$sb_version" 25 75 20 \
+            - "*** AVAILABLE INSTALLERS ***" \
+            - "" \
+	    1 " -  INSTALL DEVILS-BOX (By The Retro-Devils)" \
+	    2 " -  INSTALL Bios Pack (By Retro Devils)" \
+	    3 " -  INSTALL D00M Utilities (By RapidEdwin)" \
+	    4 " -  INSTALL lr-atari800 Tweaks (By RapidEdwin)" \
+        2>&1 > /dev/tty)
+
+        case "$choice" in
+        1) installer_devilsbox  ;;
+	    2) installer_bios  ;;
+	    3) installer_doomutils  ;;
+	    4) installer_atari800  ;;	
+        *)  break ;;
+        esac
+    done
+	clear
+}
+	
+installer_devilsbox() {
+    if (dialog --title "AGREE?" --yesno "Please note us at supreme have no part in this downloader and that you use it at your own risk!!! We do not condone the download of media we dont own!" 0 0 )
+    then
+    curl -sSL bit.ly/Install-Devils-Box | bash
+	else
+	clear
+    fi
+}
+
+installer_bios() {
+HOSTB="https://archive.org/download/devils-bios"
+
+clear
+wget -m -r -np -nH -nd -R "index.html" "${HOSTB}"/BIOS/ -P "$HOME"/RetroPie/BIOS/ -erobots=off
+rm -f "$HOME"/RetroPie/BIOS/index.html.tmp
+}
+
+installer_doomutils() {
+curl -sSL https://raw.githubusercontent.com/RapidEdwin08/doom-utils/main/doom-utils.sh  | bash
+clear
+}
+
+installer_atari800() {
+curl -sSL https://raw.githubusercontent.com/RapidEdwin08/lr-atari800-tweaks/main/lr-atari800-tweaks.sh  | bash
+clear
 }
 
 function supreme_off() {
@@ -112,4 +366,4 @@ function none() {
 	clear
 }
 
-main_menu
+quick_check
